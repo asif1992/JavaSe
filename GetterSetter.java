da24f374da455d1986b66f47a4a1ec5bd5a20675
unknown <asifmulani54@gmail.com>
2018-11-25 11:31:48 +0530

    added new class

---
+++ GetterSetter.java
@@ -0,0 +1,40 @@
+package com.javabasics.constructors;
+
+public class GetterSetter {
+	private static int x = 40;
+	public static double y = 60.0;
+
+	GetterSetter(int a, double b) {
+		x = a;
+		y = b;
+	}
+
+	GetterSetter(int a) {
+		x = a;
+
+	}
+	
+
+	GetterSetter(double b) {
+		y = b;
+	}
+
+	void add() {
+		double z = x + y;
+		System.out.println(z);
+	}
+
+	void sub() {
+		double z = x - y;
+		System.out.println(z);
+	}
+
+	int getX() {
+		return x;
+	}
+
+	void setX(int a) {
+		x = a;
+	}
+
+}
